84645400aa581de7ba07e8281dc6b6a8adaff7b5
blkydpease <blkydpease@gmail.com>
2018-11-05 21:05:26 -0800

    Initial commit with a failing test

---
+++ src/test/java/com/blkydpease/servicetwo/ServiceTwoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.blkydpease.servicetwo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceTwoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		String hell = "sadas";
+		assert(hell.equals("sadas"));
+	}
+
+}
